b2c50a55f1aeeb6e020663c82ad3337ddee7a91e
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-12-05 13:28:31 +0800

    UPDATED: the packer auto action command

--- lua/vincent/plugins-setup.lua
+++ lua/vincent/plugins-setup.lua
@@ -2,7 +2,7 @@
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
   augroup end
 ]])
 
