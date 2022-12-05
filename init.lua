0df8425a04db43025a57563f12fd1cf7e5a65cd5
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-12-05 10:35:46 +0800

    Init: the neovim setup

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("vincent.plugins-setup")
+require("vincent.core.options")
+require("vincent.core.keymaps")
+require("vincent.core.colorscheme")
---
+++ lua/vincent/plugins-setup.lua
@@ -0,0 +1,40 @@
+-- Autocommand that reloads neovim whenever you save this file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- My plugins here
+  
+  -- preferred colorscheme
+  use 'bluz71/vim-nightfly-guicolors'
+
+  -- tmux & split window navigation
+  use 'christoomey/vim-tmux-navigator'
+
+  use 'szw/vim-maximizer'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
