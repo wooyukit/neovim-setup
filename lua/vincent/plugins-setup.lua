792e1153f343389bd1f7a07f0b0c946e731627a6
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-12-15 22:21:08 +0800

    ADDED: LSP server for the nvim

--- lua/vincent/plugins-setup.lua
+++ lua/vincent/plugins-setup.lua
@@ -45,7 +45,7 @@ return require('packer').startup(function(use)
 
   -- fuzzy finding
   use ({"nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-  use ({"nvim-telescope/telescope.nvim", branch = "0.1.x" })
+  use ({"nvim-telescope/telescope.nvim", tag = "0.1.0" })
 
   -- autocompletion
   use("hrsh7th/nvim-cmp") -- completion plugin
@@ -56,6 +56,16 @@ return require('packer').startup(function(use)
   use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"}) -- snippet engine
   use("saadparwaiz1/cmp_luasnip") -- for autocompletion
   use("rafamadriz/friendly-snippets") -- useful snippets
+
+  -- managing & installing lsp servers, linters & formatters
+  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+
+  -- configuring lsp servers
+  use("neovim/nvim-lspconfig") -- easily configure language servers
+  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+  use({"glepnir/lspsaga.nvim", branch = "main"}) -- enhanced lsp uis
+  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
   -- treesitter configuration
   use({
