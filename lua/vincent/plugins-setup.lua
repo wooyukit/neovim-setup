50dbaa02f5c9d54da17652baadf24affc9b120c4
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-12-15 21:19:39 +0800

    ADDED: auto complete and code snippy for the nvim

--- lua/vincent/plugins-setup.lua
+++ lua/vincent/plugins-setup.lua
@@ -47,6 +47,16 @@ return require('packer').startup(function(use)
   use ({"nvim-telescope/telescope-fzf-native.nvim", run = "make" })
   use ({"nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
+  -- autocompletion
+  use("hrsh7th/nvim-cmp") -- completion plugin
+  use("hrsh7th/cmp-buffer") -- source for text in buffer
+  use("hrsh7th/cmp-path") -- source for file system paths
+
+  -- snippets
+  use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"}) -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
   -- treesitter configuration
   use({
     "nvim-treesitter/nvim-treesitter",
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,31 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
+  },
+  ["cmp-path"] = {
+    loaded = true,
+    path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/cmp-path",
+    url = "https://github.com/hrsh7th/cmp-path"
+  },
+  cmp_luasnip = {
+    loaded = true,
+    path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    url = "https://github.com/saadparwaiz1/cmp_luasnip"
+  },
+  ["friendly-snippets"] = {
+    loaded = true,
+    path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    url = "https://github.com/rafamadriz/friendly-snippets"
+  },
   ["gitsigns.nvim"] = {
     loaded = true,
     path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
@@ -88,6 +113,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/Users/wooyukit/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
