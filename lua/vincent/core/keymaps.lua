373dd176218528250c2b6fa03df938986aabd2e0
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2023-01-02 16:50:08 +0800

    UPDATED: the formatter and lsp config" ; ;

--- lua/vincent/core/keymaps.lua
+++ lua/vincent/core/keymaps.lua
@@ -35,3 +35,6 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- formatter
+keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.formatting()<CR>")
